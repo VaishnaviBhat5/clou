feaca2312f3d3faef2476e4b0747d632e9848068
VaishnaviBhat5 <131176243+VaishnaviBhat5@users.noreply.github.com>
2025-03-21 09:02:33 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,10 @@
 const mysql = require('mysql');
 require('dotenv').config(); // Load environment variables
 
-const con = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+  host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 
 });
 
 con.connect((err) => {
